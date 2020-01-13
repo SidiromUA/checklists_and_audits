923df24bea7043b2b956677870ef9db2dccf665f
Roman Golovatyi <roman@golovatiy.com>
2020-01-13 09:37:40 +0200

    Add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :title, length: { maximum: 40 }
+  # validates :description, length: { maximum: 500 }
 end
